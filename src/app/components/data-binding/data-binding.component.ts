160f3dc6a7641ad31f96200c9bd8514c75c2da5b
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-08-28 15:13:21 +0530

    two way data binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,27 +1,27 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
   // string,number,boolean,data
   courseName: string="Angular 18";
-
   inputType="radio";
-
   myClassName:string="bg-primary";
   rollN0:number=123;
-
   isIndian:boolean=true;
-
   currentDate:Date=new Date;
-
+  stateName:string="Madhya Pradesh";
   constructor(){
 
+  }
+  changeCourseName(){
+    this.courseName = "Reactjs";
   }
   showMessage(message:string){
     alert(message);
