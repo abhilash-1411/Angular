4cb7eae6c4fbbacd8391bb63671bcdd139b679f0
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-08-28 14:32:02 +0530

    one way data binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
+  // string,number,boolean,data
+  courseName: string="Angular 18";
 
+  inputType="radio";
+
+  myClassName:string="bg-primary";
+  rollN0:number=123;
+
+  isIndian:boolean=true;
+
+  currentDate:Date=new Date;
+
+  constructor(){
+
+  }
+  showMessage(message:string){
+    alert(message);
+  }
 }
